9dcc7060a4c7e6d791a10175b476668e9f8758e3
Irfan Badzlin <irfan.badrul11@s.unikl.edu.my>
2022-05-30 14:15:34 -0400

    add csrf protection

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'sslserver',
 ]
 
+# CSRF Protection using Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
